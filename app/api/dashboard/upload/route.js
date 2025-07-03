dc974d417f3c69d6aaa53035c3ed09b51121ce9b
tarun <r.tarunnayaka25042005@gmail.com>
2025-07-03 10:49:41 +0530

    updated schema and batch is done

--- app/api/dashboard/upload/route.js
+++ app/api/dashboard/upload/route.js
@@ -1,13 +1,18 @@
+import { auth } from "@/app/auth";
+import { prisma } from "@/app/lib/prisma-server";
 import { storeZip } from "@/app/lib/storage";
 import { NextResponse } from "next/server";
+import { data } from "react-router-dom";
 
 export async function POST(request) {
+  const session = await auth();
   const formData = await request.formData();
 
   // Get the uploaded ZIP file
   const zipFile = formData.get("imagesZip");
   const batchName = formData.get("batchName");
   const cropType = formData.get("cropType");
+  const imagesCount = formData.get("imagesCount");
 
   if (!zipFile || !zipFile.name.endsWith(".zip")) {
     return NextResponse.json(
@@ -24,7 +29,27 @@ export async function POST(request) {
       { status: 500 }
     );
   }
-
+  const DB = await prisma.batch.create({
+    data: {
+      name: batchName,
+      cropType: cropType,
+      imagesZipURL: uploadToBucket.url || `shared/${zipFile.name}`,
+      imagesCount: parseInt(imagesCount) || 0,
+      userId: session.user.id, // You need to provide the user ID
+      // The following fields have defaults, so you don't need to provide them:
+      // createdAt: new Date(), // This has @default(now())
+      // isCompletedModel: false, // This has @default(false)
+      // isCompleted: false, // This has @default(false)
+      // sessionId will get a UUID automatically
+    },
+  });
+  if (!DB) {
+    return NextResponse.json(
+      { error: "Failed to save batch data." },
+      { status: 500 }
+    );
+  }
+  console.log("✅ Batch data saved to database:", DB);
   console.log("📦 ZIP file received:", zipFile.name);
   console.log(
     "📦 ZIP file received:",
