bf77bf175ec7e95e22fda36a1e10df44ff39ce80
tarun <r.tarunnayaka25042005@gmail.com>
2025-07-24 21:06:06 +0530

    feat: integrate Swagger UI for API documentation and add swagger-ui-express dependency

--- MobileBackend/config.mjs
+++ MobileBackend/config.mjs
@@ -1,16 +1,20 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-
 import { PrismaClient } from "@prisma/client";
+import swaggerUi from "swagger-ui-express";
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const swaggerDocument = require("./swagger.json");
 
 const app = express();
 const PORT = process.env.PORT || 4000;
-
 const prisma = new PrismaClient();
 
-app.use(cors("*"));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 export { app, PORT, prisma };
